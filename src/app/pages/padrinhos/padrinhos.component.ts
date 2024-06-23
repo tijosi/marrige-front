f2639f471f29dd3b50d80f8cf42726de04a7eee2
Edson Martins <edsondizeseis@hotmail.com>
2024-06-23 03:44:41 -0300

    Inicio da Page Padrinhos

--- src/app/pages/padrinhos/padrinhos.component.ts
+++ src/app/pages/padrinhos/padrinhos.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class PadrinhosComponent {
 
+    dsPadrinhos: any[] = [
+        {nome: 'Dhavid Paes barbosa',   imagemUrl: '../../../assets/photos-banner/foto1.png'},
+        {nome: 'Uanderson Balbino',     imagemUrl: '../../../assets/photos-banner/foto2.png'},
+        {nome: 'Carlos Eduardo',        imagemUrl: '../../../assets/photos-banner/foto3.png'},
+        {nome: 'Silvano Barcelos',      imagemUrl: '../../../assets/photos-banner/foto4.png'},
+        {nome: 'Leonardo Guedes',       imagemUrl: '../../../assets/photos/edson.png'},
+    ]
+
 }
